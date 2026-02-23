42b822efa6f945d9e1c6ed2113684a00f910fb83
sreehari27 <sreehari27jan@gmail.com>
2026-02-23 23:32:45 +0530

    designing product preview page. image preview and change image work donne.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
 
 function Product() {
-  return (
-    <div>
-      Product
-      
+
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
+
+  const getProductData = async () => {
+
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null
+      }
+    })
+  }
+
+  useEffect(() => {
+    getProductData();
+  }, [productId]);
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img className='w-full h-auto' src={image} alt="" />
+          </div>
+        </div>
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2 '>{productData.name}</h1>
+          <div className='flex item-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <span className='pl-2 text-xs text-gray-500'>(120 reviews)</span>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency} {productData.price}</p>
+          <p className='mt-5 md:w-4/5 text-gray-600'>{productData.description}</p>
+        </div>
+
+      </div>
+
+    </div>
+  ) : (
+    <div className='opacity-0'>
+
     </div>
   )
 }
